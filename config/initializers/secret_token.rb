ae255ccd15a85a35479f9bf5a81c76de30fa62e6
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2013-01-14 20:33:25 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ziyuan::Application.config.secret_token = 'e2ccc1b7545b6b2bea2e13e164487567ad92e62aab47f80f03728a847b28574b07dbde63dd152f5850180863a5f8a745c9830ea0212da3026449f4b32f322b23'
